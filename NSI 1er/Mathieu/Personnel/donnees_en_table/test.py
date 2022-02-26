398caad2b6e80bf6e8f5765a9c9ca311a475ab4d
Nicolas Toro <61847756+nt-games-ytb@users.noreply.github.com>
2022-02-26 14:39:27 +0100

    Add files via upload

---
+++ NSI 1er/Mathieu/Personnel/donnees_en_table/test.py
@@ -0,0 +1,31 @@
+import csv
+
+def import_csv(fichier):
+    lecteur = csv.DictReader(open(fichier + '.csv', 'r'))
+    print([dict(ligne) for ligne in lecteur])
+    #return [dict(ligne) for ligne in lecteur ]
+
+def import_csv2(fichier):
+    resultat = []
+    with open(fichier + '.csv',newline = '') as csvfile:
+        s = csv.reader(csvfile,delimiter = ';')
+        for i in s:
+            resultat.append(i)
+    print(resultat)
+    #return resultat 
+  
+def vers_csv(nom, ordre):
+    with open(nom + '.csv', 'w') as fic:
+        dic = csv.DictWriter(fic, fieldnames = ordre)
+        table = eval(nom)
+        dic.writeheader() # première ligne, celle des attributs
+    for ligne in table:
+        dic.writelow(ligne) # ajoute les lignes de la table)
+    return None
+
+folder = "D:/NSI_1er/Mathieu/Personnel/donnees_en_table/"
+#import_csv("tortues")
+#import_csv("D:/NSI_1er/Mathieu/Personnel/donnees_en_table/tortues")
+#import_csv("NSI_1er/Mathieu/Personnel/donnees_en_table/tortues")
+import_csv(folder + "tortues")
+import_csv2(folder + "tortues")
